6d10f75c59558caaa63357a6245746fc4a3f279e
fclopez4 <fc.lopez@uniandes.edu.co>
2024-09-26 13:36:05 -0500

    feat: se agrga el maquetado inicial de perfil usuario

--- src/app/modules/kikirik/pages/perfil-usuario/perfil-usuario.component.ts
+++ src/app/modules/kikirik/pages/perfil-usuario/perfil-usuario.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/modules/login/services/login.service';
+import { OptionsConfirm } from 'src/app/shared/models/dialog-confirm-options.model';
+import { DialogConfirmServiceService } from 'src/app/shared/services/dialog-confirm-service.service';
 
 @Component({
   selector: 'app-perfil-usuario',
   templateUrl: './perfil-usuario.component.html',
   styleUrls: ['./perfil-usuario.component.scss'],
 })
-export class PerfilUsuarioComponent  implements OnInit {
+export class PerfilUsuarioComponent  {
 
-  constructor() { }
+  constructor(
+    private dialogConfirmServiceService: DialogConfirmServiceService,
+    private LoginService: LoginService,
+    private router: Router,
+  ) { }
 
-  ngOnInit() {}
+  async showDeleteAccountModal() {
+    let options: OptionsConfirm = {
+      tituloBtnConfirmar: 'Si',
+      tituloBtnCancelar: 'No',
+      width: '280px',
+    }
+    let confirmacion = await this.dialogConfirmServiceService.succesConfirmMessaje(
+      '¿Esta seguro de eliminar la cuenta?<br>Este cambio es irreversible',
+      options);
+    console.log(confirmacion);
+    if (confirmacion) {
+      this.logout()
+    }
+  }
+
+  private logout() {
+    this.LoginService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
